7b753e2bb8be01bee0509b08caac19d2a4293d9f
phusomnia <144326634+phusomnia@users.noreply.github.com>
2025-06-28 15:12:16 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 sudo apt update
 
 # # [Firmware]
-sudo apt install firmware-linux-nonfree alsa-utils
+sudo apt install firmware-linux-nonfree alsa-utils -y
 
 # # [WM]
-sudo apt install i3 xorg 
+sudo apt install i3 xorg -y
 
 # # [Audio]
-sudo apt install pavucontrol pulseaudio
+sudo apt install pavucontrol pulseaudio -y
 
 # # [Browser]
 sudo apt update && sudo apt install extrepo -y
 sudo extrepo enable librewolf
-sudo apt update && sudo apt install librewolf
+sudo apt update && sudo apt install librewolf -y
 curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/edge stable main" | sudo tee /etc/apt/sources.list.d/microsoft-edge.list > /dev/null
 sudo apt update
-sudo apt install microsoft-edge-stable
+sudo apt install microsoft-edge-stable -y
 
-sudo apt install rofi thunar network-manager kitty nitrogen
+sudo apt install rofi thunar network-manager kitty nitrogen -y
 
