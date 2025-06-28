fcd0ab8ae57e55a357905856ade9d85c408cde43
phusomnia <phusomnia@phusomnia.phusomnia.xyz.com>
2025-06-28 11:12:24 +0700

    [Update]

---
+++ install.sh
@@ -0,0 +1,22 @@
+sudo apt update
+
+# # [Firmware]
+sudo apt install firmware-linux-nonfree alsa-utils
+
+# # [WM]
+sudo apt install i3 xorg 
+
+# # [Audio]
+sudo apt install pavucontrol pulseaudio
+
+# # [Browser]
+sudo apt update && sudo apt install extrepo -y
+sudo extrepo enable librewolf
+sudo apt update && sudo apt install librewolf
+curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/edge stable main" | sudo tee /etc/apt/sources.list.d/microsoft-edge.list > /dev/null
+sudo apt update
+sudo apt install microsoft-edge-stable
+
+sudo apt install rofi thunar network-manager kitty
+
