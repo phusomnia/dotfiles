6449a64fb31a844b4f0073c63a89319104e4625e
phusomnia <144326634+phusomnia@users.noreply.github.com>
2025-08-31 08:36:01 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,3 +34,8 @@ sudo apt install rofi -y
 
 # [Image]
 sudo apt install nitrogen -y
+
+# [Dotnet]
+curl -L https://dot.net/v1/dotnet-install.sh -o dotnet-install.sh
+chmod +x ./dotnet-install.sh
+./dotnet-install.sh --channel 9.0
