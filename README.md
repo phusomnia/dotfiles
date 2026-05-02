ee3b1980fe1f47597cd0014cd42c34283b3884fa
phusomnia <phusomnia@phusomnia.xyz>
2026-05-03 02:14:24 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# dotfiles
+# Dotfiles (inspired by p3nguin-kun)
+> 
+
+# Details
+
+- Linux 
+    - Window Manager: [GlazeWM]()
+    - Terminal: 
+        - [Terminal]()/[LF]()
+    - Browser: 
+        - Edge
+        - Brave
+    - FileManger: 
+        - Ranger
+        - Thunar
+    - TextEditor: 
+        - Windsurf
+        - Neovim
+    - Music Player: 
+        - Musikcube
+    - Monitor: 
+        - Neofetch
+        - Htop
+    - Languages: 
+        - Java ()
+        - JS/TS (Bun)
+        - C# (Dotnet)
+        - Go
+        - Nim
+        - 
+- Windows 
+    - Window Manager: [GlazeWM]()
+    - Terminal: [Terminal]()/[LF]()
+    - Browser: [Edge]()/[Brave]()
+    - FileManger: [LF]()
+    - TextEditor: [Vscode]()/[Neovim]()
+    - Music Player: [Musikcube]()/[Mpv]()
+    - Fetch: [Neofetch]()
--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
-sudo apt update
-
 # [Firmware]
-sudo apt install firmware-linux-nonfree alsa-utils -y
+sudo apt update && sudo apt install firmware-linux-nonfree alsa-utils -y
 
 # [WM]
-sudo apt install i3 xorg lightdm thunar -y
+sudo apt update && sudo apt install i3 xorg lightdm thunar -y
 
 # [Audio]
-sudo apt install pavucontrol pulseaudio -y
+sudo apt update && sudo apt install pavucontrol pulseaudio -y
 
 # [Browser]
 sudo apt update && sudo apt install extrepo -y
@@ -17,8 +15,21 @@ sudo apt update && sudo apt install librewolf -y
 curl -O mes.deb "https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_138.0.3351.55-1_amd64.deb"
 sudo dpkg -i microsoft-edge-stable_138.0.3351.55-1_amd64.deb
 
+# [Code]
 curl -L -o code.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
 sudo dpkg -i code.deb
+
+# [Windsurf]
+sudo apt-get install wget gpg
+wget -qO- "https://windsurf-stable.codeiumdata.com/wVxQEIWkwPUEAGf3/windsurf.gpg" | gpg --dearmor > windsurf-stable.gpg
+sudo install -D -o root -g root -m 644 windsurf-stable.gpg /etc/apt/keyrings/windsurf-stable.gpg
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/windsurf-stable.gpg] https://windsurf-stable.codeiumdata.com/wVxQEIWkwPUEAGf3/apt stable main" | sudo tee /etc/apt/sources.list.d/windsurf.list > /dev/null
+rm -f windsurf-stable.gpg
+
+sudo apt install apt-transport-https
+sudo apt update
+
+sudo apt install windsurf -y
 
 # [Network]
 sudo apt install network-manager -y 
@@ -39,3 +50,56 @@ sudo apt install nitrogen -y
 curl -L https://dot.net/v1/dotnet-install.sh -o dotnet-install.sh
 chmod +x ./dotnet-install.sh
 ./dotnet-install.sh --channel 9.0
+echo 'export DOTNET_ROOT=$HOME/.dotnet' >> ~/.bashrc
+echo 'export PATH=$PATH:$HOME/.dotnet' >> ~/.bashrc
+source ~/.bashrc
+
+# [Temurin (Open java)]
+sudo apt install -y wget apt-transport-https gpg
+wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/adoptium.gpg > /dev/null
+echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
+
+sudo apt install -y temurin-21-jdk
+
+# [Nim]
+curl https://nim-lang.org/choosenim/init.sh -sSf | sh
+
+# [Go]
+mkdir -p ~/.go
+wget https://go.dev/dl/$(curl -s https://go.dev/VERSION?m=text | head -1).linux-amd64.tar.gz -O ~/.go/go.tar.gz
+
+rm -rf ~/.go/go
+tar -C ~/.go -xzf ~/.go/go.tar.gz
+
+echo 'export PATH=$PATH:$HOME/.go/go/bin' >> ~/.bashrc
+source ~/.bashrc
+
+# [Bun]
+curl -fsSL https://bun.sh/install | bash
+
+# [Ollama]
+curl -fsSL https://ollama.com/install.sh | sh
+
+# [Docker engine]
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# [Other deps]
+sudo apt update
+sudo apt install libgl1-mesa-dev libglu1-mesa-dev libx11-dev libxcursor-dev libxinerama-dev libxrandr-dev libxi-dev
